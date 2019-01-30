de77e83a62c80b16f190ce9ce36e5344087ad204
Dennis Bunfield <dennis@factoid.org>
2019-01-30 14:25:18 -0600

    updated to support firmware v 1.5.5

--- factomize-ledger.sh
+++ factomize-ledger.sh
@@ -5,8 +5,8 @@
 #echo $DIR
 
 #first delete the MFW app
-python -m ledgerblue.deleteApp --targetId 0x31100003 --appName "Factom"
+#python -m ledgerblue.deleteApp --targetId 0x31100003 --appName "Factom"
 
 #then install the MFW app
 
-python -m ledgerblue.loadApp --appFlags 0x40 --path "44'/131'" --path "44'/132'" --path "44'/143165576'" --curve secp256k1 --tlv --targetId 0x31100003 --delete --fileName /opt/ledger-toolchain/bin/app.hex --appName Factom --appVersion 1.1.5 --dataSize 0x00000040 --icon 0100000000ffffff00ffffffffffffffff3ff03ff03fff3fff0ff83fff3fff3fffffffffffffffffff
+python -m ledgerblue.loadApp --appFlags 0x40 --path "44'/131'" --path "44'/132'" --path "44'/143165576'" --curve secp256k1 --tlv --targetId 0x31100004 --delete --fileName /opt/ledger-toolchain/bin/app.hex --appName Factom --appVersion 1.1.5 --dataSize 0x00000040 --icon 0100000000ffffff00ffffffffffffffff3ff03ff03fff3fff0ff83fff3fff3fffffffffffffffffff
