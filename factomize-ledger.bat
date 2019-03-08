2b314258e02cbbdfa290edecfbc0edfc0c2a35a3
bunfield <dbunfield@cybitron.com>
2019-03-08 09:09:44 -0600

    updated to comply with slip44 request

--- factomize-ledger.bat
+++ factomize-ledger.bat
@@ -9,4 +9,4 @@ rem python -m ledgerblue.deleteApp --targetId 0x31100003 --appName "Factom"
 
 rem then install the MFW app
 
-python -m ledgerblue.loadApp --appFlags 0x40 --path "44'/131'" --path "44'/132'" --path "44'/143165576'" --curve secp256k1 --tlv --targetId 0x31100003 --delete --fileName bin\app.hex --appName Factom --appVersion 1.1.5 --dataSize 0x00000040 --icon 0100000000ffffff00ffffffffffffffff3ff03ff03fff3fff0ff83fff3fff3fffffffffffffffffff
+python -m ledgerblue.loadApp --appFlags 0x40 --path "44'/131'" --path "44'/132'" --path "44'/281'" --curve secp256k1 --tlv --targetId 0x31100003 --delete --fileName bin\app.hex --appName Factom --appVersion 1.2.1 --dataSize 0x00000040 --icon 0100000000ffffff00ffffffffffffffff3ff03ff03fff3fff0ff83fff3fff3fffffffffffffffffff
