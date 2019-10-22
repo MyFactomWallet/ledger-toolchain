e774b906be9ddf96b6de0adaa229dbccc7a215b2
Dennis Bunfield <dennis@factoid.org>
2019-10-22 10:37:51 -0500

    added missing quotes to install script and bumped version number

--- factomize-ledger.sh
+++ factomize-ledger.sh
@@ -9,4 +9,5 @@ fi
 echo Follow the instructions on the ledger Nano S scrren.
 
 #sideload the factom ledger app.
-python -m ledgerblue.loadApp --appFlags 0x240 --path 44'/131' --path 44'/132' --path 44'/281' --curve secp256k1 --tlv --targetId 0x31100004 --delete --fileName bin/app.hex --appName Factom --appVersion 1.3.0 --dataSize 0x00000040 --icon 0100000000ffffff00ffffffffffffffff3ff03ff03fff3fff0ff83fff3fff3fffffffffffffffffff
+python -m ledgerblue.loadApp --appFlags 0x240 --path "44'/131'" --path "44'/132'" --path "44'/281'" --curve secp256k1 --tlv --targetId 0x31100004 --delete --fileName bin/app.hex --appName Factom --appVersion 1.3.1 --dataSize 0x00000040 --icon 0100000000ffffff00ffffffffffffffff3ff03ff03fff3fff0ff83fff3fff3fffffffffffffffffff
+
